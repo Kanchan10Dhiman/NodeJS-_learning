33d7ef30b3e06d4d27aea9e6835ede3a770f76df
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2025-09-05 00:43:20 +0530

    added content

--- 4exportMultipleFunVar.js
+++ 4exportMultipleFunVar.js
@@ -11,12 +11,39 @@ If you reassign it again (module.exports = mult), the previous value (add) is re
 */
 //INDIVIDUALLY EXPORT & REQUIRE MULTIPLE FUNCTIONS.
 //yha add,mult hi naam likhna h jo aap.js me h or console me b same hi likhna h ..add,mult
-const{add, mult}=require("./math")
-console.log(add(5,2));
-console.log(mult(5,2));
+// const{add, mult}=require("./math")
+// console.log(add(5,2));
+// console.log(mult(5,2));
 //---------------------------------
 
 //EXPORTING AND REQUIRING MUTLIPLE FUNCTIONS AT ONCE
+
+// const{add, mult, div,sub, pi}=require("./math")
+
+//ANOTHER METHOD-->best method
+const math=require("./math")
+console.log(math.add(5,2));
+console.log(math.mult(5,2));
+console.log(math.div(6,2));
+console.log(math.pi);
+/*
+yha destructuring ho rhi h
+-->Destructuring = object ke andar ke properties ko alag-alag variables me tod dena.
+-->Instead of writing:
+
+const math = require("./math");
+console.log(math.add(5,2));
+console.log(math.mult(5,2));
+
+-->Tum directly likh sakti ho:
+
+const { add, mult, div, sub } = require("./math");
+console.log(add(5,2));   // 7
+console.log(mult(5,2));  // 10
+console.log(div(6,2));   // 3
+console.log(sub(6,2));   // 4
+
+*/
 
 
 
--- math.js
+++ math.js
@@ -5,12 +5,25 @@ const add=(a,b)=>{
 const mult=(a,b)=>{
     return a*b;
 };
+const sub=(a,b)=>{
+    return a-b;
+};
+const div=(a,b)=>{
+    return a/b;
+};
+const pi=3.124;
 // module.exports=add;
 // module.exports=mult;
-
+//----------------------------------------------
 //iNDIVIDUALLY EXPORT & REQUIRE MULTIPLE FUNCTIONS
-module.exports.add=add;
-module.exports.mult=mult;
+// module.exports.add=add;
+// module.exports.mult=mult;
+//---------------------------------------
+
+//EXPORTING AND REQUIRING MUTLIPLE FUNCTIONS AT ONCE
+module.exports={ add,mult,sub,div,pi};
+
+
 
 
 

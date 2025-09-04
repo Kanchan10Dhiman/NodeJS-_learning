5e06f33c9f0ceaf32aeb1139df148963f03a18f2
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2025-09-04 14:20:44 +0530

    added text

--- modules.js
+++ modules.js
@@ -9,3 +9,35 @@ Think of it like this:
 
 console.log("studying about modules");
 //if we write 'node modules.js' it cant access the the file's data in basic.js...for that we have to use export/import
+//----------------------------------------------------------------
+/*
+🔹 Encapsulation in Node.js-->Apna code ek box (module) ke andar rakhna taaki wo global scope ko pollute na kare
+
+Node.js by default CommonJS module system use karta hai.
+Iska matlab hai:
+
+-->Har file ek module hoti hai.
+
+-->Jo bhi cheez (function, variable, class) tum dusri file me use karna chahti ho, usko tumhe module.exports se bahar bhejna hoga.
+
+-->Aur dusri file me use karne ke liye require() karna hoga.
+-----------------------------------------------------------------
+WHY USEFUL?
+
+-->No pollution of global scope
+Agar modules na hote, to har jagah variables/functions global ban jaate aur naam clash ho jaata.
+
+-->Code reusability
+Tum ek file ka code dusri file me reuse kar sakti ho.
+
+-->Maintainability
+Large project ko chhote-chhote modules me tod ke maintain karna easy ho jaata hai.
+
+*/
+
+
+/*------------------------------------------------------------------
+MODULES ARE FILS JO HUM BANTE H, APP.JS ,MATH.JS etc--OR HME EXPORT KRNA PDTA H JO B ACCESSIBLE KRWANA H ANOTHER MODULE ME.
+Agar math.js me app.js ka content chahiye, to export app.js me hoga aur require/import math.js me.
+-------------------------------------------------------------------
+*/

a2d454a889586665149cfc72619c56b3c29ffa00
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2025-09-06 15:36:00 +0530

    added file

---
+++ 7FS_module.js
@@ -0,0 +1,6 @@
+//The fs module allows us to interact with the file system on our computer.You can create, read, update, delete files and directories.
+//CRUD-create,read,update,del
+
+//ACCESS FS MODULE
+const fs=require("fs");
+

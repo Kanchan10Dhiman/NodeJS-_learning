7a612388d9bef2a8b12280d4be21543ed60de039
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2025-09-09 20:04:28 +0530

    added file

--- 10FS_AsyncAwait.js
+++ 10FS_AsyncAwait.js
@@ -1,1 +1,26 @@
-//ASYNC-AWAIT 
+//ASYNC-AWAIT -->ye functions k sath hi use ho skte h 
+
+//read folder or to get the folder's file list
+// const readfolder= async ()=>{
+//     try{
+//         await fs.promises.readdir(filepath);
+//     }
+//     catch(err) {
+//         console.error(err);
+//     }
+// };
+
+const fs=require("fs/promises");
+const path=require("path");
+
+const filename="ayncAwait.txt";
+const filepath=path.join(__dirname, filename);
+
+//CREATE FILE USING AYNC-AWAIT
+
+
+
+
+
+
+

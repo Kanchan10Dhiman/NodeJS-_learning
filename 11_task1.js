8fd179fdd89a19fe2546ceada5cfbf55edc94e38
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2025-09-11 22:52:41 +0530

    modifies

--- 11_task1.js
+++ 11_task1.js
@@ -1,1 +1,17 @@
 //a simple task related to EventEmitter
+/* create a program using node.js EventEmitter that:
+//OBJECTIVE
+-->listens for multiple types of user events(loin,logout,purchase, profile update)
+-->tracks how many times each event is emitted
+-->logs a summary of all event occurrences when a special summary eent is triggered.
+
+//REQUIREMENT
+-->create atleast four custom events(user-login,logout,purchase, profile update)
+-->emit these events multiple times  with diff arguemnets(eg username.purchased  item)
+-->track and store the count of each event type
+-->define a summary event that logs a detailed report of how many times each event was triggered
+*/
+const EventEmitter=requires("event");
+
+
+

6d6d6a7a7e26bc8fdc82ca5871d6b01dc73d0414
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2025-09-12 00:10:02 +0530

    completed half code

--- 11_task1.js
+++ 11_task1.js
@@ -11,7 +11,32 @@
 -->track and store the count of each event type
 -->define a summary event that logs a detailed report of how many times each event was triggered
 */
-const EventEmitter=requires("event");
+const EventEmitter=require("events");
+const emitter=new EventEmitter();
+
+//EVENT LISTENER
+emitter.on("user-login", (username)=>{
+    console.log(`${username} logged in`);
+});
+
+emitter.on("user-logout" ,(username)=>{
+    console.log(`${username} logged out`)
+});
+
+emitter.on("item-purchased", (item)=>{
+    console.log(`item puschased by user : ${item}`);
+});
+
+emitter.on("update-profile", (update, field)=>{
+    console.log(`update data is ${field} from ${update}`)
+});
+
+
+//emit\trigger events
+emitter.emit("user-login", "kanchan Dhiman");
+emitter.emit("item-purchased", "laptop" );
+emitter.emit("update-profile", "kanchan", "kanchu");
+emitter.emit("user-logout", "kanchan");
 
 
 
